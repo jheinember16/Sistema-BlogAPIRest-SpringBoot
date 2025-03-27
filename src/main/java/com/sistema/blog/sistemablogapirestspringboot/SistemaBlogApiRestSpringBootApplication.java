86e07782dfc5842d8884f4c33f7294bf32de1ab9
Jheinember <jheinemberstithjn@ufps.edu.co>
2025-03-27 00:23:33 -0400

    Subiendo archivos

---
+++ src/main/java/com/sistema/blog/sistemablogapirestspringboot/SistemaBlogApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.blog.sistemablogapirestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBlogApiRestSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBlogApiRestSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sistema/blog/sistemablogapirestspringboot/SistemaBlogApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.blog.sistemablogapirestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBlogApiRestSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
